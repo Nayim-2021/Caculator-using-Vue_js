7adbc5993a73aeedc1e6ad27da207c83e11dadd0
Nayim Ali Khan <79267860+Nayim-2021@users.noreply.github.com>
2022-10-07 19:01:24 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Caculator/>
+  </div>
+</template>
+
+<script>
+import Caculator from './components/Caculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Caculator
+  }
+}
+</script>
+
+<style>
+
+</style>
